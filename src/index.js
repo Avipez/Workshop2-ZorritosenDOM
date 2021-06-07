004542be4d0ad94c3c63082c54e91f6ee963a05e
David Lopez <dg_lf@hotmail.com>
2021-06-07 14:43:50 -0500

    Workshop 2 terminado zorritos felices

--- src/index.js
+++ src/index.js
@@ -1,6 +1,39 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import { registerImage } from "./lazy.js";
+import { createImageNodes } from "./utils.js";
 
-console.log('Happy hacking :)')
+
+//Cargar imagenes default
+
+const allImages = document.querySelectorAll("img[data-src]")
+console.log(allImages)
+allImages.forEach(registerImage)
+
+// Agregar nuevas imagenes
+const imageContainer = document.querySelector("#images");
+const button = document.querySelector("button[type='submit']");
+button.addEventListener("click", () => {
+  const [node, image] = createImageNodes();
+  registerImage(image);
+  imageContainer.append(node);
+});
+
+
+//borrar
+
+const clean = document.querySelector("button[type='reset']")
+clean.addEventListener("click", () => {
+  imageContainer.innerHTML = "";
+});
+
+
+
+const removeButton = document.querySelector('#Remove')
+removeButton.className = 'text-white px-3 py-2 rounded-lg bg-red-900 focus:outline-none';
+
+const removeImage = () => {
+        const lastImage = imageContainer.lastChild;
+        imageContainer.removeChild(lastImage);
+
+}
+
+removeButton.addEventListener('click', removeImage);
